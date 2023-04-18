ee299867d7445ac8d2d08d36a3ca7865718a179e
Kristi Leka <m.kristileka@gmail.com>
2023-04-18 22:28:42 +0200

    root directory change

--- src/main/scala/kristileka/zsh/Main.scala
+++ src/main/scala/kristileka/zsh/Main.scala
@@ -1,13 +1,16 @@
 package kristileka.zsh
 
 import scala.io.StdIn.readLine
-
 import kristileka.zsh.state.State
+
+import java.io
 
 object Main extends App {
   var state = State()
-  while (true) {
-    print("-> tmp ")
+  val RED = "\u001B[31m"
+ while (true) {
+    print(Console.GREEN + "➜ ")
+    print(Console.BLUE + s"${state.currentDirectory} ")
     val command = readLine()
     state = state.evaluate(command)
   }
--- src/main/scala/kristileka/zsh/common/Const.scala
+++ src/main/scala/kristileka/zsh/common/Const.scala
@@ -1,5 +1,5 @@
 package kristileka.zsh.common
 
 object Const {
-  val ROOT_DIRECTORY = "tmp"
+  val ROOT_DIRECTORY = "~"
 }
